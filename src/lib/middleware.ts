214d087c42e2162c6773cc50ac0af34fe23508f6
Tellapalli Akhil <tellapallyakhil89@gmail.com>
2026-03-02 17:48:14 +0530

    fix: allow public access to SEO files in middleware logic

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -38,6 +38,10 @@ export async function updateSession(request: NextRequest) {
         !user &&
         !request.nextUrl.pathname.startsWith('/login') &&
         !request.nextUrl.pathname.startsWith('/auth') &&
+        !request.nextUrl.pathname.endsWith('.xml') &&
+        !request.nextUrl.pathname.endsWith('.txt') &&
+        !request.nextUrl.pathname.endsWith('.html') &&
+        !request.nextUrl.pathname.endsWith('.json') &&
         request.nextUrl.pathname !== '/'
     ) {
         // no user, potentially respond by redirecting the user to the login page
