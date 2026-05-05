ba225a97907fb164b3e45eb8ad727125108c89b5
Tellapalli Akhil <tellapallyakhil89@gmail.com>
2026-05-05 23:22:27 +0530

    Add sleek animated ScrollToTop navigation component

--- src/components/layout/Providers.tsx
+++ src/components/layout/Providers.tsx
@@ -4,11 +4,13 @@ import { DreamCompanyProvider } from '@/context/DreamCompanyContext';
 import dynamic from 'next/dynamic';
 
 const GlobalJokes = dynamic(() => import('@/components/features/GlobalJokes'), { ssr: false });
+const ScrollToTop = dynamic(() => import('@/components/features/ScrollToTop'), { ssr: false });
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <DreamCompanyProvider>
             <GlobalJokes />
+            <ScrollToTop />
             {children}
         </DreamCompanyProvider>
     );
